d312d384536c178d9cc8b1573a88aa1a50f9a3c2
OmKumarChaudhary <omkgithub@gmail.com>
2025-12-15 18:32:15 +0000

    Add onboarding screen and update gitattributes

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreen();
+}
+
+class _OnboardingScreen extends State<OnboardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
